f4b7f3742d298fe8cfdbf897693021e052c7f56f
tsaiiii08 <lintsaiting0824@gamil.com>
2023-07-02 16:34:18 +0800

    add function for checking account and password

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express =require('express')
 const mogoose = require('./config/mongoose')
+const exphbs = require('express-handlebars')
+const Login = require('./models/login')
+const bodyParser = require('body-parser') 
+
 const app = express()
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true })) 
 
 app.get('/',(req,res)=>{
-  res.send('hello')
+  res.render('index')
+})
+
+app.post('/login',(req,res)=>{
+  const user = req.body
+  Login.findOne({'email':user.email})
+  .lean()
+  .then(data=>{
+    if(data!== null){
+      if(data.password === user.password){
+        console.log(data)
+        return res.render('logined',{firstName: data.firstName})
+      }
+      else {
+        const passwordError = true
+        return res.render('index', { passwordError })
+      }
+    } else {
+      const error = true
+      return res.render('index',{error})
+    }
+  })
+
 })
 
 app.listen(3000, (req, res)=>{
